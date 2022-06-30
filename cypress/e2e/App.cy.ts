f8efb1f56f96addade2234bba6092499425a2327
Leebonggu <leebonggu12@hanmail.net>
2022-06-30 18:07:17 +0900

    gogogogogo

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -2,16 +2,16 @@ Cypress.config();
 
 describe('initialize App', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:4173');
 
   })
 
   it('Render Data', () => {
-  //   cy.intercept({
-  //     method: 'GET',
-  //     url: '/user',
-  //     hostname: 'localhost',
-  //   }).as('apiCheck')
+    cy.intercept({
+      method: 'GET',
+      url: '/user',
+      hostname: 'localhost',
+    }).as('apiCheck')
   
     // cy.wait('@apiCheck')
     cy.get('.main').contains('loading...')
